05db060cdde4de77b32bea68f5a40e846e422c0b
Daniela Resende <danielaresende26@users.noreply.github.com>
2026-03-24 13:06:50 -0300

    fix: permite x-turnstile-token no cabecalho cors

--- api/consultar.js
+++ api/consultar.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Origin', '*'); // Permite conexões do github.io
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
-    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+    res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Turnstile-Token');
 
     // Navegadores fazem um "aviso prévio" de segurança chamado OPTIONS. 
     // Precisamos sempre aprovar para o CORS funcionar!
